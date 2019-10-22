2224a4b601a7e5a6c613ef4e8fa56a67f7af4dcc
Jesse Byers <jesse.s.byers@gmail.com>
2019-10-22 18:11:47 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,8 +1,6 @@
 class Cat
-<<<<<<< HEAD
-=======
+
   attr_accessor :owner, :mood
->>>>>>> c4c2c11a85409358d19682720075346482d51ab3
   attr_reader :name
   @@all = []
 
@@ -17,7 +15,6 @@ class Cat
     @name
   end
 
-<<<<<<< HEAD
   def owner
     @owner
   end
